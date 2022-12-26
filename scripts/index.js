8b23f806a927cae79591d62cf023fb709e5b401f
Vadim Koenen <lekovad@gmail.com>
2022-12-26 22:01:28 +0300

    6

--- scripts/index.js
+++ scripts/index.js
@@ -9,29 +9,18 @@ const elements = document.querySelector('.elements');
 const elementLikeButton = document.querySelectorAll('.element__like-button');
 const formElement = document.forms.addProfile;
 
+
 //открытие попапа
 profileEditButton.addEventListener('click', (event) => {
     popup.classList.add('popup_open');
 });
 
-  //функция открытия попапов
-  function popupOpen(popup) {
-    popup.classList.add('popup_open');
-  };
-  
 // внесение данных в форму и редактирование профиля
 formElement.addEventListener('submit', (event) => {
     event.preventDefault();
     profileName.textContent = formElement.elements.addName.value;
     profileDescription.textContent = formElement.elements.addDescription.value;
 });
-
-function handleFormProfileSubmit(evt) {
-    evt.preventDefault();
-    profileName.textContent = nameInputProfile.value;
-    profileProfession.textContent = jobInputProfile.value;
-    closePopup(popupProfile);
-  }
 
 //закрытие попапа
 popupCloseButton.addEventListener('click', (event) => {
@@ -46,36 +35,6 @@ popup.addEventListener('click', (event) => {
 popupSaveButton.addEventListener('click', (event) => {
     popup.classList.remove('popup_open');
 });
-
-toggleModal();
-
-
-// функция попапа профиля
-
-
-  //функция закрытия попапов
-  function closePopup(popups) {
-    popups.classList.remove('popup_open');
-  };
-
-
-
-// слушатели попапа профиля
-formProfile.addEventListener('submit', handleFormProfileSubmit);
-
-popupProfileCloseButton.addEventListener('click', function () {
-  closePopup(popupProfile);
-});
-
-profileButton.addEventListener('click', function () {
-  openPopup(popupProfile);
-  nameInputProfile.value = profileName.textContent;
-  jobInputProfile.value = profileProfession.textContent;
-});
-
-
-
-
 
 //кнопка лайк
 
