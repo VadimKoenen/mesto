c9798c171c8ebc3a6babce2d62789cd971379edc
Vadim Koenen <lekovad@gmail.com>
2023-01-29 18:39:40 +0300

    popup  done

--- scripts/index.js
+++ scripts/index.js
@@ -1,50 +1,39 @@
-
 const profileEditButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = document.querySelector('.popup__close-button');
 const profileName = document.querySelector('.profile__name');
 const profileDescription = document.querySelector('.profile__description');
-const popupSaveButton = document.querySelector('.popup__save-button');
-const elements = document.querySelector('.elements');
-const elementLikeButton = document.querySelectorAll('.element__like-button');
+const profileNameInpit = popup.querySelector('[name="addName"]');
+const profileDescriptionInput = popup.querySelector('[name="addDescription"]');
 const formElement = document.forms.addProfile;
 
-
 //открытие попапа
-profileEditButton.addEventListener('click', (event) => {
+function openPopup() {
     popup.classList.add('popup_open');
-});
-
-// внесение данных в форму и редактирование профиля
-formElement.addEventListener('submit', (event) => {
-    event.preventDefault();
-    profileName.textContent = formElement.elements.addName.value;
-    profileDescription.textContent = formElement.elements.addDescription.value;
-});
+};
 
 //закрытие попапа
-popupCloseButton.addEventListener('click', (event) => {
+function closePopup() {
     popup.classList.remove('popup_open');
-});
+};
 
+// внесение данных в форму и редактирование профиля
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+    profileName.textContent = profileNameInpit.value;
+    profileDescription.textContent = profileDescriptionInput.value;
+    closePopup();
+};
+
+//event-listeners
+profileEditButton.addEventListener('click', (event) => {
+    profileNameInpit.value = profileName.textContent;
+    profileDescriptionInput.value = profileDescription.textContent;
+    openPopup();
+});
+formElement.addEventListener('submit', handleFormSubmit);
+popupCloseButton.addEventListener('click', closePopup);
 popup.addEventListener('click', (event) => {
     if (event.target === event.currentTarget)
-        popup.classList.remove('popup_open');
+        closePopup;
 });
-
-popupSaveButton.addEventListener('click', (event) => {
-    popup.classList.remove('popup_open');
-});
-
-//кнопка лайк
-
-for (var i = 0; i < elementLikeButton.length; i++) {
-    elementLikeButton[i].addEventListener('click', like);
-}
-
-function like() {
-    this.classList.toggle('element__like-button_active');
-}
-
-
-
